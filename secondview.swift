c4c5c06e79290af90e4e7a079e59bf7b78cacdf3
Medha Manda <medhamanda9@gmail.com>
2025-07-10 10:26:00 -0400

    added second view

---
+++ secondview.swift
@@ -0,0 +1,18 @@
+//
+//  secondview.swift
+//  navigation
+//
+//  Created by Medha Manda on 7/10/25.
+//
+
+import SwiftUI
+
+struct secondview: View {
+    var body: some View {
+        Text("this is the scond view!")
+    }
+}
+
+#Preview {
+    secondview()
+}
