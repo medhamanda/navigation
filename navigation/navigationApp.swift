638533eb50e07f632e6c78734a3457f15934fe0c
Medha Manda <medhamanda9@gmail.com>
2025-07-10 10:00:50 -0400

    Initial Commit

---
+++ navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by Medha Manda on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
